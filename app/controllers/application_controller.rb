2d594b86c46999e46dba87a260edeb474645a18e
github username <github email address>
2019-01-11 17:54:05 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,38 +14,5 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  get '/signup' do
-    if !!session[:user_id]
-      redirect '/tweets/tweets'
-    end
-    erb :'/users/create_user'
-  end
-
-  post '/signup' do
-    if params[:username] == "" || params[:email] == "" || params[:password] == ""
-      redirect '/signup'
-    end
-    
-    @user = User.create(params)
-    session[:user_id] = @user.id
-    redirect '/tweets/tweets'
-  end
-
-  get '/login' do
-    erb :'/users/login'
-  end
-
-  post '/login' do
-    @user = User.find_by(:username => params[:username])
-    if @user != nil && @user.password == params[:password]
-      session[:user_id] = @user.id
-      binding.pry
-
-      redirect '/tweets'
-    end
-    redirect '/signup'
-  end
- 
-
 end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,38 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    if !!session[:user_id]
+      redirect '/tweets'
+    end
+    erb :'/users/create_user'
+  end
+
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect '/signup'
+    end
+    
+    @user = User.create(params)
+    session[:user_id] = @user.id
+    redirect '/tweets'
+  end
+
+  get '/login' do
+    erb :'/users/login'
+  end
+
+  post '/login' do
+    @user = User.find_by(:username => params[:username])
+
+    # if @user != nil && @user.authenticate(params[:password])
+    if @user 
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    end
+    redirect '/signup'
+  end
+ 
+  
+
 
 end
