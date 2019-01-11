06967b8444562b3b88118c3ed61becad04bebaa7
github username <github email address>
2019-01-11 09:47:21 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,47 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
   end
 
+  
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    if !!session[:user_id]
+      redirect 'tweets/tweets'
+    end
+    erb :'users/create_user'
+  end
+
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect '/signup'
+    end
+    
+    @user = User.create(params)
+    session[:user_id] = @user.id
+    redirect 'tweets/tweets'
+  end
+
+  get '/login' do
+    erb :'users/login'
+  end
+
+  post '/login' do
+    # @user = User.find_by(username: params[:username], password: params[:password])
+    @user = User.find_by(params)
+    if @user
+          session[:user_id] = @user.id
+          redirect 'tweets/tweets'
+    end
+    binding.pry
+    redirect '/signup'
+  end
+ 
+
 end
+
